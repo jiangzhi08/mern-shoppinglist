79e502a2760aad19cda6a8f782015f2fb42ab46e
z j <jiangzhi08@gmail.com>
2020-07-12 13:54:29 -0500

    dsaf

--- server.js
+++ server.js
@@ -17,23 +17,23 @@ mongoose
 const itemsRouter = require('./routes/api/items')
 app.use('/api/items',itemsRouter)
 
-// if(process.env.NODE_ENV === 'production') {
-//     app.use(express.static('client/build'))
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
 
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-//     })
-// }
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
-  // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-  }
-  
+// if (process.env.NODE_ENV === 'production') {
+//     // Serve any static files
+//     app.use(express.static(path.join(__dirname, 'client/build')));
+//   // Handle React routing, return all requests to React app
+//     app.get('*', function(req, res) {
+//       res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//     });
+//   }
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, ()=> {
